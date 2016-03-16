8f5aed72b850dfc7548aa96d2efa6e8a97569be3
Buktop <vitekosipov@gmail.com>
2016-03-16 22:28:30 +0300

    v0.5

--- js/script.js
+++ js/script.js
@@ -8,19 +8,25 @@ $(document).ready(function() {
     var articlecount = 2;
     var articles = '';
     var notices = '';
+    var notice = 2;   
+    function ProgressBar(max,current) {
+        var a = ((current/max)*100-2) + '%';
+        $('#progress').css({'width': a});
+    }
+  ProgressBar(1000, 470);
     for (var i = 1; i <= articlecount; i++) {
-        articles += '<div id="' + i + '"></div>';
+        articles += '<div class="ArtField" id="' + i + '"></div>';
     }
     $(".content").html(articles);
     function createElem(id,title,article,imgUrl){
-            var currentArticle = '<div class="ArtTitle">'+article+'</div>';
-            var currentTitle = '<div>'+title+'</div>';
-            var currentImgUrl = '<div>'+imgUrl+'</div>';
-            $(".content #"+id).html(currentTitle+currentArticle+currentImgUrl);
+            var currentArticle = '<div class="ArtText">'
+            currentArticle += '<div class="ArtTitle">'+article+'</div>';
+            currentArticle += '<div>'+title+'</div>';
+            currentArticle += '<div>'+imgUrl+'</div></div>';
+            $(".content #"+id).html(currentArticle);
     }
     createElem(1,"Заголовок","текст","URL");
     createElem(2,"Заголовок","текст","URL");   
-    var notice=2;   
     for (var i = 1; i <= notice; i++) {
         notices += '<div class="not" id="not' + i + '"></div>';
     }
@@ -32,8 +38,11 @@ $(document).ready(function() {
     }
     createNotice(1,"blalalal");  
     createNotice(2,"FUCKU");
+    if (notice == 0) $(".notice").hide();
     $(".exet").on("click", function(event) {
         var target = event.target;
-    $($(target).parent()).hide();
-        })  
+        $($(target).parent()).hide();
+        notice--;
+        if (notice == 0) $(".notice").hide();
+    })  
 });
