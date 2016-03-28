9b701f9eb1cc6072eba0a2b2b4f230bf03f3b665
white-beard <maximsen@mail.ru>
2016-03-28 12:48:47 +0300

    Added files via upload

---
+++ js/profile.js
@@ -0,0 +1,39 @@
+function include(url) {
+  var script = document.createElement('script');
+  script.src = url;
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
+include ("jquery-2.2.1.min.js");
+$(document).ready(function() {
+	        $('#photoload').hide();
+
+	$.getJSON('js/profile.json', function(data){
+		$('#fam').val(data.fam);
+		$('#name').val(data.name);
+		$('#middleName').val(data.middleName);
+		$('#age').val(data.age);
+		$('#fon').val(data.fon);
+		$('#mail').val(data.mail);
+		$('.image img').attr('src', data.photo);
+
+		});			
+	$('body').on('click','.image',function(event){				
+			$('input[type=file]').click();		
+			});
+						
+	function readURL(input) {
+             if (input.files && input.files[0]) {
+              var reader = new FileReader();
+               reader.onload = function (e) {
+               $('.image img').attr('src', e.target.result);
+               } 
+             reader.readAsDataURL(input.files[0]);
+		}
+		}
+		
+			$("#photoload").change(function(){			
+			
+			readURL(this);
+	
+			});
+});
